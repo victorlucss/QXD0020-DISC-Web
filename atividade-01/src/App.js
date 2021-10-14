4ef992ff77e2c4b32608167243d98ca6a660e63a
Victor Lucas <victor.lucas@ifood.com.br>
2021-10-14 14:12:59 -0300

    feat: atividade 01

---
+++ atividade-01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Victor from './components/Victor';
+
+function App() {
+  return (
+    <div className="App">
+      <Victor />
+    </div>
+  );
+}
+
+export default App;
---
+++ atividade-01/src/components/Victor.jsx
@@ -0,0 +1,14 @@
+
+const Victor = () => {
+  return (
+    <>
+      <strong>Nome Completo:</strong> Victor Lucas da Silva Monteiro<br />
+      <strong>Idade</strong> 23<br />
+      <strong>Curso</strong> Engenharia de Software<br />
+      <strong>Cidade de Origem</strong> Fortaleza - CE<br />
+      <strong>E-mail usado na Disciplina</strong> victor.lucas@ifood.com.br
+    </>
+  )
+}
+
+export default Victor;
